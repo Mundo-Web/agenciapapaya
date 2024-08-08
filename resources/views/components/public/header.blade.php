20fd96110e91565cb91dcd9a9a437e3ba607d944
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-08-08 10:04:08 -0500

    final

--- resources/views/components/public/header.blade.php
+++ resources/views/components/public/header.blade.php
@@ -1,15 +1,15 @@
 {{-- style="background-image: url('{{ asset('images/img/textura_roja.webp') }}');" --}}
 <header class="bg-transparent top-0 h-20">
-    <div class="flex flex-col md:flex-row justify-between items-center w-11/12 mx-auto py-5 gap-5">
+    <div class="flex flex-col md:flex-row justify-center items-center w-11/12 mx-auto py-5 gap-5">
         <div class="flex justify-center items-center">
             <a href="{{route('index')}}">
                 <img src="{{ asset('images/svg/logoblancoheader.svg') }}" alt="Hidromec" class="w-full" />
             </a>
         </div>
-        <div class="hidden md:flex justify-center items-center w-full md:w-auto">
+        {{-- <div class="hidden md:flex justify-center items-center w-full md:w-auto">
             <a href="#formContactos"
                 class="btn-secondary">Solicitar Servicio</a>
-        </div>
+        </div> --}}
     </div>
 
     <div class="flex justify-end w-11/12 mx-auto  z-10">
